cd30ce1154708ad93335cabf2dd9208160951b5b
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-27 04:55:50 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,12 +4,10 @@ def reformat_languages(languages)
   languages.each do |styles, langs|
     languages[styles].each do |lang, types|
       languages[styles][lang].each do |type, value|
-        if !new_hash.has_key?(lang)
           new_hash[lang] = {
             type: value,
             style: []
           }
-        else
           new_hash[lang][:style].push(styles)
         end
       end
