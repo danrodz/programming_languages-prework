a2375d0b8e7a12a382a08c5c28ec9afaef5b26cd
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-27 05:05:15 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,11 +1,11 @@
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  new_hash.default = false 
+  new_hash.default = "false" 
   languages.each do |styles, langs|
     languages[styles].each do |lang, types|
       languages[styles][lang].each do |type, value|
-          if new_hash[lang] == false
+          if new_hash[lang] == "false"
           new_hash[lang] = {
             type: value,
             style: []
