b2bd7b336fa9c59cea99256696098bb3e44b95a0
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-27 04:45:24 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,7 +9,7 @@ def reformat_languages(languages)
             type: value,
             style: []
           }
-          
+          new_hash[lang][:style].push(styles)
         #end
       end
     end
